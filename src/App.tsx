6e5134c7cc8e1fae5c6df728e73dcaebcd9d8e5c
Egor Mikheev <egor.mikheev@humaniq.com>
2022-04-12 10:34:22 +0300

    feature/CORE-236-add-style-system

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React, { useEffect } from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
-import "./App.scss";
+import "./App.sass";
 import routes from "./utils/routes";
 import { ETHProvider } from "./stores/provider/providerStore";
 
 import b from "buffer";
-import { WalletNotConnected } from "./components/app/WalletNotConnected";
-import { ProviderNotFound } from "./components/app/ProviderNotFound";
 import { Main } from "./screens/main/Main";
 
 window.Buffer = b.Buffer;
